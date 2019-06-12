f5a1df1bf0ed7421d64cb85958aab1be9be22336
bednik <32166304+bednik@users.noreply.github.com>
2019-06-12 13:34:32 +0200

    Legger til prosjektfilene

---
+++ src/main/java/Karaktergutta/GradesSort/GradesSortApplication.java
@@ -0,0 +1,13 @@
+package Karaktergutta.GradesSort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradesSortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradesSortApplication.class, args);
+	}
+
+}
